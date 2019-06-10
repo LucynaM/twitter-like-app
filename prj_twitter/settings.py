62b2aa0f426cb68affe64568884d138cc2f8bf03
Lucyna Mazur <lucyna@studiokreator.com>
2019-06-10 20:42:52 +0200

    add login path

--- prj_twitter/settings.py
+++ prj_twitter/settings.py
@@ -120,6 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+LOGIN_URL = 'twitter:login'
+LOGIN_REDIRECT_URL = 'twitter:entries'
 
 AUTH_USER_MODEL	= 'twitter.MyUser'
 
